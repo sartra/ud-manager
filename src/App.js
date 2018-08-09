1f91a8245c350edaf87146d10a6d80e40fd37882
Sarah Renshaw <srenshaw@nycl137457m.us.dci.discovery.com>
2018-08-09 16:48:58 -0400

    added firebase and common components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native'; 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
+import reducers from './reducers'; 
+import firebase from 'firebase'; 
 
 class App extends Component {
+
+    componentWillMount(){
+      // Initialize Firebase
+      const config = {
+        apiKey: "AIzaSyC8XwMPnu38QnRkgXyLsTLeR7Dc3RHmbP8",
+        authDomain: "manager-26e44.firebaseapp.com",
+        databaseURL: "https://manager-26e44.firebaseio.com",
+        projectId: "manager-26e44",
+        storageBucket: "manager-26e44.appspot.com",
+        messagingSenderId: "1031671567419"
+      };
+      firebase.initializeApp(config);
+    }
+
+
   render() {
     return (
-      <Provider store={ createStore() }>
+      <Provider store={ createStore(reducers) }>
         <View>
           <Text> Hello! </Text>
         </View>
